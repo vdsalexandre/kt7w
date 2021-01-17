85fc816e98f937da457f09ec444bdde356cf8693
vdsalex <alexandre.vandensteen@gmail.com>
2021-01-17 16:40:24 +0100

    updating request mapping "mycocktail"

--- src/main/kotlin/com/vds/kt7w/controller/ApiController.kt
+++ src/main/kotlin/com/vds/kt7w/controller/ApiController.kt
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController
 import java.util.*
 
 @RestController
-@RequestMapping("/7w")
+@RequestMapping("/mycocktail")
 class ApiController {
     companion object {
         private val LOGGER = LogManager.getLogger()
