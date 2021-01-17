917c6a0ab2552cf3fa8e79004815246adee7c130
vdsalex <alexandre.vandensteen@gmail.com>
2021-01-17 12:11:51 +0100

    adding log4j config and adding logger on ApiController.kt

--- src/main/kotlin/com/vds/kt7w/controller/ApiController.kt
+++ src/main/kotlin/com/vds/kt7w/controller/ApiController.kt
@@ -1,17 +1,23 @@
 package com.vds.kt7w.controller
 
+import com.vds.kt7w.model.Cocktail
 import com.vds.kt7w.model.Hello
 import com.vds.kt7w.repository.CocktailRepository
+import org.apache.logging.log4j.LogManager
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 import java.util.concurrent.atomic.AtomicInteger
 
 @RestController
 @RequestMapping("/7w")
 class ApiController {
+    companion object {
+        private val LOGGER = LogManager.getLogger()
+    }
 
     @Autowired
     lateinit var repository: CocktailRepository
@@ -23,5 +29,14 @@ class ApiController {
         Hello(index.incrementAndGet(), "Hello $name !")
 
     @GetMapping("/cocktail/all")
-    fun findAll() = repository.findAll()
+    fun findAll(): List<Cocktail> {
+        LOGGER.info("GET: /cocktail/all")
+        return repository.findAll()
+    }
+
+    @GetMapping("/cocktail/{id}")
+    fun findById(@PathVariable("id") id: Long): Optional<Cocktail> {
+        LOGGER.info("GET: /cocktail/$id")
+        return repository.findById(id)
+    }
 }
