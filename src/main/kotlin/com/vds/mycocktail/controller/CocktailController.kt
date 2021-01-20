217b86b2a3824336dbdbf70a23ba2a6bc79be547
vdsalex <alexandre.vandensteen@gmail.com>
2021-01-20 20:59:46 +0100

    adding request add and delete cocktail

--- src/main/kotlin/com/vds/mycocktail/controller/CocktailController.kt
+++ src/main/kotlin/com/vds/mycocktail/controller/CocktailController.kt
@@ -4,8 +4,11 @@ import com.vds.mycocktail.model.Cocktail
 import com.vds.mycocktail.repository.CocktailRepository
 import org.apache.logging.log4j.LogManager
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.Optional
@@ -30,5 +33,17 @@ class CocktailController {
     fun findCocktailById(@PathVariable("id") id: Long): Optional<Cocktail> {
         LOGGER.info("GET: /cocktail/$id")
         return cocktailRepository.findById(id)
+    }
+
+    @PostMapping("/add")
+    fun saveCocktail(@RequestBody cocktail: Cocktail) {
+        LOGGER.info("POST: /cocktail/add -> $cocktail")
+        cocktailRepository.save(cocktail)
+    }
+
+    @DeleteMapping("/del/{id}")
+    fun delCocktail(@PathVariable("id") id: Long) {
+        LOGGER.info("DELETE: /cocktail/del/$id")
+        cocktailRepository.deleteById(id)
     }
 }
